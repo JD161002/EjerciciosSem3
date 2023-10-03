4fb968c5bb6c40374c67267bae9ee362bca15551
AndreeVillena <andrecito.v.a@hotmail.com>
2023-10-03 11:03:43 -0500

    cambio

--- EjerciciosSem3/Program.cs
+++ EjerciciosSem3/Program.cs
@@ -17,6 +17,7 @@ namespace EjerciciosSem3
             {
 
                 Console.WriteLine("---------------------------------------------------");
+                Console.WriteLine("LISTAS");
                 Console.WriteLine("1. Ingresar nuevo dato lista 1");
                 Console.WriteLine("2. Ingresar nuevo dato lista 2");
                 Console.WriteLine("3. Listar elementos de la lista 1 (inicio al fin)");
